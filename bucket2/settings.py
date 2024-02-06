38db5828924aa5f1d183876f409daa95101af9f4
jesseturner21 <jesse.turner2021@gmail.com>
2024-02-06 09:35:45 -0700

    css and logout

--- bucket2/settings.py
+++ bucket2/settings.py
@@ -15,7 +15,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+LOGIN_URL = '/profile/login/'
+LOGOUT_REDIRECT_URL = '/'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
--- profiles/urls.py
+++ profiles/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import edit_user_profile, register, user_login, home, create_content
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('edit/', edit_user_profile, name='edit_user_profile'),
     path('new_slip/', create_content, name='new_slip'),
     path('register/', register, name='register'),
     path('login/', user_login, name='user_login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', home, name='home'),
 ]
